276fc2daf9d03b46ff90f74892f6adc3660a9fad
Rafael J. Vicente <rafaelj.vicente@upm.es>
2023-03-06 15:49:51 +0100

    Updated version

--- src/setuptools_cpp_cuda/__init__.py
+++ src/setuptools_cpp_cuda/__init__.py
@@ -6,7 +6,7 @@ from .build_ext import BuildExtension, fix_dll
 from .extension import CppExtension, CudaExtension, CUDA_HOME, CUDNN_HOME
 from .find_cuda import find_cuda_home, find_cuda_home_path
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __all__ = [
     'BuildExtension', 'CppExtension', 'CudaExtension',
     'find_cuda_home', 'find_cuda_home_path',
